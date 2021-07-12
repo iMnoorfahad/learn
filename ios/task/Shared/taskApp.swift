dab53e85d352c77222e5101b033511855938c13a
Noor Fahad <noor@drillmaps.com>
2021-07-12 21:48:18 +0530

    IOS Task

---
+++ ios/task/Shared/taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskApp.swift
+//  Shared
+//
+//  Created by Noor Fahad on 07/01/21.
+//
+
+import SwiftUI
+
+@main
+struct taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
